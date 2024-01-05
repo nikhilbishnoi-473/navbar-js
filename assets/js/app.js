274899e2252df9327487866e9a129ae6f6124f1b
nikhilbishnoi-473 <nikhilbishnoibhadu@gmail.com>
2024-01-04 12:12:07 -1200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+const menuIcon = document.querySelector(".menuIcon")
+const menulist = document.querySelector(".menulist")
+
+menuIcon.addEventListener("click", function() {
+    menulist.classList.toggle("show");
+    document.body.classList.toggle("overFlowHidden")
+    menuIcon.classList.toggle("show")
+}) 
+ 
